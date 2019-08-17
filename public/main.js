a5129102c5e07b9b1f4288bf7f35dc28766de9cd
Adam Jones <adam@linuxtampa.com>
2019-08-17 14:21:09 -0400

    Done with CSS and started calls to second API

--- public/main.js
+++ public/main.js
@@ -6,11 +6,25 @@ const main = () => {
 
     .then(data => {
       console.log(data)
+      document.querySelector('#copyright').textContent =
+        'copyright: ' + data.copyright + ' | title: ' + data.title
+      document.querySelector('#first-api').src = data.url
     })
+  showNasaData()
 }
 
-const showData = () => {
-  document.querySelector('#display').textContent = data.copyrigth
+const showNasaData = () => {
+  fetch('https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming')
+    .then(resp => {
+      return resp.json()
+    })
+
+    .then(launches => {
+      console.log(launches)
+      document.querySelector('#shuttle').textContent
+      document.querySelector('.mission-name').textContent =
+        launches[0].mission_name
+    })
 }
 
 document.addEventListener('DOMContentLoaded', main)
